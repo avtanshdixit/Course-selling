2a9537981c755382cb95bfc63631480abe51b1fd
Avtansh Dixit <avtansh.di@gmail.com>
2026-01-22 12:18:59 +0530

    Initial backend setup for course selling app

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+
+app.post("/user/signup", function(req,res){
+
+})
+
+app.post("/user/signup", function(req,res){
+
+})
+
+app.get("/user/purchases", function(req,res){
+
+})
+
+app.get("/course/purchase", function(req,res){
+
+})
+
+app.get("/courses", function(req,res){
+
+})
+
+
+app.post("/userlogin", function(req,res){
+
+})
+
+app.listen(3000);
